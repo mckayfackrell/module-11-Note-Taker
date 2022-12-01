39d1d89c194081d34d1da72f700f68eee623df66
“mckayfackrell” <mvfackrell@gmail.com>
2022-12-01 15:28:03 -0700

    added nodemon

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,21 @@
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
+const app = express();
+const PORT = process.env.port || 3001;
 
-// const app = express();
-// const PORT = 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/api', api),
 
-// app.use(express.static('public'));
+app.use (express.static(path.join(__dirname, 'public')));
 
-// app.get('/'(req, res) => 
-//   res.sendFile(path.join(__dirname,
-//     'public/index.html')
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+app.listen(PORT, () =>
+  console.log(`Listening on port http://localhost:${PORT}`)
+);
